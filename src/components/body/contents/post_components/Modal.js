b39e75986b391e2eeb00dc12d195a8f8f3dee0d2
ggamsang812 <ggamsang812@gmail.com>
2023-09-20 00:24:48 -0400

    modal update #1

--- src/components/body/contents/post_components/Modal.js
+++ src/components/body/contents/post_components/Modal.js
@@ -1,14 +1,26 @@
 import React from "react";
+import "./Modal.css";
 
 function Modal({ post, closeModal }) {
+  // Replace newline characters with <br /> elements
+  const formattedDescription = post.description.replace(/\n/g, "<br />");
+
+  // Use dangerouslySetInnerHTML to render HTML with line breaks
   return (
     <div className="modal">
+      <button className="modal-close-button" onClick={closeModal}>
+        X
+      </button>
       <div className="modal-content">
-        <h2>{post.title}</h2>
+        <h2 className="modal-title">{post.title}</h2>
         <p>{post.date}</p>
-        <p>{post.description}</p>        
-        {/* <img src={post.image} alt="Post Image" /> */}
-        <button onClick={closeModal}>Close</button>
+        <p
+          className="modal-description"
+          dangerouslySetInnerHTML={{ __html: formattedDescription }}
+        ></p>
+        {post.image && (
+          <img className="modal-image" src={post.image} alt="Post" />
+        )}
       </div>
     </div>
   );
--- src/components/body/contents/post_components/PostData.js
+++ src/components/body/contents/post_components/PostData.js
@@ -1,59 +1,66 @@
+const importAll = (r) => {
+  let images = {};
+  r.keys().forEach((key) => (images[key] = r(key)));
+  return images;
+};
+
+const images = importAll(
+  require.context(
+    "./post_images", // Specify the directory where your images are located
+    false, // Whether subdirectories should be searched too
+    /\.(png|jpe?g|gif)$/ // Specify the file extensions of the images you want to include
+  )
+);
+
 const initialPosts = [
-  
   // Add more posts as needed
   {
     title: "Google Resume Workshop - Attended.",
-    description: "This is the description of Post 0.",
+    description:
+      "This is the description of Post 0. dddddddddddddd\n dddddddddd dddddddddddd ddddddddddddddddddd dddddddddddddddddddddd \naaaaaaaaaa ddddddddddddddddd dddddddddddddd dddddddd",
     date: "2023-09-18",
-    image: "path_to_image_0.jpg",
   },
   {
     title: "MyBlogYL Project - Started.",
-    description: "This is the description of Post 0.",
+    description:
+      "To practice my React.js knowledge and skill, I'm start a project to build my personal blog \nThis is a new line. <a href='https://github.com/ggamsang812' target='_blank'>Visit Example</a>",
     date: "2023-09-02",
-    image: "path_to_image_0.jpg",
   },
   {
     title: "AWS Certified Solutions Architect - Associate Exam - Prep.",
     description: "This is the description of Post 1.",
     date: "2023-08-17",
-    image: "path_to_image_1.jpg",
   },
   {
     title: "Recovered. Back to Work!",
     description: "This is the description of Post 2.",
     date: "2023-08-16",
-    image: "path_to_image_2.jpg",
   },
   {
     title: "AWS Summit New York 2023 - Attended.",
     description: "This is the description of Post 2.",
     date: "2023-07-26",
-    image: "path_to_image_2.jpg",
+    image: images["./20230919_AWS_Summit.jpg"], // Use the imported image
   },
   {
     title: "Learning Flutter and Dart.",
     description: "This is the description of Post 3.asdfasdfasdfasdfasdfdsf",
     date: "2023-05-27",
-    image: "path_to_image_3.jpg",
   },
   {
     title: "AWS Certified Cloud Practitioner Exam - Passed!",
     description: "This is the description of Post 4.",
     date: "2023-05-26",
-    image: "path_to_image_4.jpg",
   },
   {
     title: "AWS Certified Cloud Practitioner Exam - Prep.",
     description: "This is the description of Post 5.",
     date: "2023-04-26",
-    image: "path_to_image_5.jpg",
   },
   {
     title: "Finally, Learning How to Use GitHub. No More Avoiding GitHub!",
     description: "This is the description of Post 6.",
     date: "2023-04-25",
-    image: "path_to_image_2.jpg",
   },
 ];
 
